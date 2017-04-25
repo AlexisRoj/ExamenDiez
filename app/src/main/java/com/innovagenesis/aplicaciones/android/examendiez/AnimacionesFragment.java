d4237e773b2274f2a725dd507b84b4f3a7a06588
AlexisRoj <alexisroj2@gmail.com>
2017-04-24 22:53:15 -0500

    Initial commit

---
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examendiez/AnimacionesFragment.java
@@ -0,0 +1,51 @@
+package com.innovagenesis.aplicaciones.android.examendiez;
+
+
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AnimacionesFragment extends Fragment {
+
+    View view;
+
+    public AnimacionesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        view = inflater.inflate(R.layout.fragment_animaciones, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view2) {
+
+                View estrella = view.findViewById(R.id.estrella);
+                Animation animation = AnimationUtils.loadAnimation(getActivity(),R.anim.animation_estrella);
+                estrella.startAnimation(animation);
+
+            }
+        });
+
+        return view;
+    }
+
+
+}
---
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examendiez/DiccionarioUtilidades.java
@@ -0,0 +1,17 @@
+package com.innovagenesis.aplicaciones.android.examendiez;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+
+/**
+ * Created by alexi on 24/04/2017.
+ */
+
+public class DiccionarioUtilidades extends Fragment {
+
+
+
+
+}
