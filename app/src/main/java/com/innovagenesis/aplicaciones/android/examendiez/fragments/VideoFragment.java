80ad4a3bb27a8a0b730f6e1fc3fd8f36415e4c1f
AlexisRoj <alexisroj2@gmail.com>
2017-04-27 15:27:28 -0500

    Funcionando modulo de video

--- app/src/main/java/com/innovagenesis/aplicaciones/android/examendiez/fragments/VideoFragment.java
+++ app/src/main/java/com/innovagenesis/aplicaciones/android/examendiez/fragments/VideoFragment.java
@@ -1,15 +1,28 @@
 package com.innovagenesis.aplicaciones.android.examendiez.fragments;
 
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.innovagenesis.aplicaciones.android.examendiez.R;
+
+import java.io.File;
+
+import static android.app.Activity.RESULT_OK;
+import static com.innovagenesis.aplicaciones.android.examendiez.R.string.audio;
+import static com.innovagenesis.aplicaciones.android.examendiez.R.string.video;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +31,10 @@ public class VideoFragment extends Fragment implements View.OnClickListener {
 
     Button buttonAbrirVideo;
     Button buttonGuardarVideo;
-    VideoView videoView;
+    private VideoView videoView;
+    private Uri videoUri;
+    private static int REQUEST_CODE_VIDEO = 1;
+    static final int Pick_video = 1;
 
     public VideoFragment() {
         // Required empty public constructor
@@ -37,6 +53,8 @@ public class VideoFragment extends Fragment implements View.OnClickListener {
 
         buttonAbrirVideo.setOnClickListener(this);
         buttonGuardarVideo.setOnClickListener(this);
+        videoView = (VideoView)view.findViewById(R.id.videoView_video);
+
 
         return view;
     }
@@ -44,15 +62,87 @@ public class VideoFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()){
+        switch (v.getId()) {
 
-            case R.id.abrir_video:{
+            case R.id.abrir_video: {
+
+                Intent intent = new Intent();
+                intent.setType("audio/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Selecciona un audio"), Pick_video);
+
                 break;
             }
-            case R.id.capturar_video:{
+            case R.id.capturar_video: {
+                /*
+                 * Encargado de grabar video y reproducirlo
+                 * */
+                Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                File videosFolder = new File(Environment.getExternalStorageDirectory(), "VideosNextU");
+                videosFolder.mkdirs();
+                File video = new File(videosFolder, "video.mp4");
+                videoUri = Uri.fromFile(video);
+                videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, videoUri);
+                startActivityForResult(videoIntent, REQUEST_CODE_VIDEO);
                 break;
             }
         }
 
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+      /*  switch (requestCode) {
+            case Pick_video:
+                if (resultCode == RESULT_OK) {
+                    String patch = data.getDataString();
+
+                    try {
+                        videoView = new VideoView(getContext());
+                        videoView.setVideoURI(Uri.parse(patch));
+
+
+                    } catch (Exception e) {
+                        Toast.makeText(getContext(), "Erro al ejecutar el audio", Toast.LENGTH_SHORT).show();
+                        videoView.start();
+                    }
+                }
+        }*/
+
+        mGrabarVideo(requestCode, resultCode);
+    }
+
+    /**
+     * Método encargado de grabar los videos y almacenarlos
+     * */
+    private void mGrabarVideo(int requestCode, int resultCode) {
+        if (requestCode == REQUEST_CODE_VIDEO && resultCode == RESULT_OK) {
+            Toast.makeText(getContext(),
+                    "Se guardó el video en:\n" + Environment.getExternalStorageDirectory() +
+                            "/VideosNextU/video.mp4", Toast.LENGTH_LONG).show();
+
+            MediaController mediaController = new MediaController(getContext());
+            videoView.setMediaController(mediaController);
+            videoView.setVideoURI(videoUri);
+            videoView.start();
+
+            mediaController.setAnchorView(videoView);
+
+            videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    //videoView.start();
+                }
+            });
+
+        } else {
+            Toast.makeText(getContext(),
+                    "Ha ocurrido un error al guardar el video", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
+
+
